20059f238b07840fa5d0a516360d73f41dc9c34d
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-26 05:46:13 +0100

    backend

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const app = express();
 
+const user = [];
+
+app.use(express.urlencoded({ extended: false }));
+
 // routes
 app.get("/", (req, res) => {
   res.render("index.ejs");
