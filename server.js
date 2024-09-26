4a61c6eeede3b5a2a6d46a142373b43f8dfb5621
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-26 05:12:14 +0100

    backedn

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// importing installed libraries
+
 const express = require("express");
 const app = express();
 
+// routes
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
@@ -12,5 +15,6 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+// end routes
 
 app.listen(3000);
