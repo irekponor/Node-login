9a873f04d55a6fc0d1428d6e5fed655017c03c93
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-27 02:30:11 +0100

    bcrypt

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post("/register", async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash();
+    const hashedPassword = await bcrypt.hash(req.body.Password);
   } catch {}
 });
 
