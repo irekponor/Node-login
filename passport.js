4b620279bc9cfe8f5e60fb5f8abe7ea77935339c
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-28 02:00:42 +0100

    passport.js

--- passport.js
+++ passport.js
@@ -4,17 +4,21 @@ const bcrypt = require("bcrypt");
 function initialize(passport) {
   // users authentication
   const authenticateUsers = async (email, password, done) => {
-
     // get users by email
     const user = getUserByEmail(email);
     if (user == null) {
       return done(null, false, { message: "User not found" });
     }
     try {
-        if (await)
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      }
     } catch (e) {
-        
+      console.log(e);
+      return done(e);
     }
-
   };
+
+  // the usernamefield can be customized to whatever you want
+  passport.use(new LocalStrategy({ usernameField: "email" }));
 }
