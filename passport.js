75c9fc54f8d8e557e6ba3604b14275391ea7a5a8
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-28 01:50:10 +0100

    passport.js

--- passport.js
+++ passport.js
@@ -1,4 +1,12 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 
-function initialize(passport) {}
+function initialize(passport) {
+  // users authentication
+  const authenticateUsers = async (email, password, done) => {
+    const user = getUserByEmail(email);
+    if (user == null) {
+      return done(null, false, { message: "User not found" });
+    }
+  };
+}
