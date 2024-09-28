70fc538a79a45bc906a9e0887f462d1ea16a7bcb
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-28 04:22:40 +0100

    ejs

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const initializePassport = require("./passport");
 const flash = require("express-flash");
 const session = require("express-session");
 
-initializePassport(passport, (email) =>
-  users.find((user) => user.email === email)
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
 const users = [];
